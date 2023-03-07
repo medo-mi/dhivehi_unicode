e30b6dfa5d4e6f4d0c6e1cfcfaacb213f2ea1bc5
Mohamed Ismail <modeissey@gmail.com>
2023-03-08 02:36:38 +0500

    1.0.3

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.0.2'
+VERSION = '1.0.3'
 DESCRIPTION = 'Python Dhivehi (Thaana) ASCII, accent to unicode converter'
-LONG_DESCRIPTION = 'Python package to convert Dhivehi (Thaana) ASCII and accent text to Unicode'
+#LONG_DESCRIPTION = 'Python package to convert Dhivehi (Thaana) ASCII and accent text to Unicode'
 
 # Setting up
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
 setup(
     name="dhivehi_unicode",
     version=VERSION,
     author="Mohamed Ismail (medo)",
     author_email="<modeissey@gmail.com>",
     description=DESCRIPTION,
-    url="git@github.com:medo-mi/dhivehi_unicode.git"
+    url="https://github.com/medo-mi/dhivehi_unicode",
     long_description_content_type="text/markdown",
-    long_description=LONG_DESCRIPTION,
+    long_description=long_description,
     packages=find_packages(),
     install_requires=[],
     keywords=['Dhivehi', 'Thaana'],
